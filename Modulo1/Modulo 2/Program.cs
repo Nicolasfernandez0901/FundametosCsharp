87560cc59e5b0736e0b7f330bbdb695ef1a826fa
Nicolas Fernadnez <124635622+Nicolasfernandez0901@users.noreply.github.com>
2023-02-08 19:33:03 -0500

    Inicio csharp
    Aprendiendo

---
+++ Modulo1/Modulo 2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Modulo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre");
+            string usuario = Console.ReadLine();
+            Console.WriteLine("El nombre del usuario es: " + usuario);
+        }
+    }
+}
---
+++ Modulo1/Modulo1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            int edad = 5;
+            Console.WriteLine(edad);
+            string mensaje = "Holaaa";
+            bool bandera = false;
+            var numero =  1;
+            int num1;
+
+        }
+        }
+    }
