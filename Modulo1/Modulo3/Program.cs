4b9e63b6ab53b908eb1fa4bafddefef0f852e75d
Nicolas Fernadnez <124635622+Nicolasfernandez0901@users.noreply.github.com>
2023-02-08 20:25:02 -0500

    Create Program.cs

---
+++ Modulo1/Modulo3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Modulo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSuperGrande = int.MaxValue;
+            Console.WriteLine(numeroSuperGrande);
+            float altura = 180.72f;
+            double anchura = 45.123;
+            decimal monto = 9.99m;
+            //Para la proxima clase determinar eñ maximo valor
+            //de el tipo de dato float, decimal y double
+            float numeroDuperGrande = float.MaxValue;
+            Console.WriteLine(numeroDuperGrande);
+            double numeroJuperGrande = double.MaxValue;
+            Console.WriteLine(numeroJuperGrande);
+            decimal numeroHiperGrande=decimal.MaxValue;
+            Console.WriteLine(numeroHiperGrande);
+        }
+    }
+}
